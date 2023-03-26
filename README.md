fd71501ec0db7cf5c18ff75ac2b0feed05894788
JesperDramsch <JesperDramsch@users.noreply.github.com>
2023-03-26 00:32:34 +0000

    deploy: 0ce4536245c03eadfacf21b71766760fbe9d3654

---
+++ README.md
@@ -0,0 +1,76 @@
+## Python Deadlines [![pages-build-deployment](https://github.com/JesperDramsch/python-deadlines/actions/workflows/pages/pages-build-deployment/badge.svg?branch=gh-pages)](https://github.com/JesperDramsch/python-deadlines/actions/workflows/pages/pages-build-deployment)
+
+Countdown timers to keep track of a bunch of Python conference deadlines.
+
+## Contributing
+
+Contributions are very welcome!
+
+To add or update a deadline:
+- Fork the repository
+- Update `_data/conferences.yml`
+- Make sure it has the `title`, `year`, `id`, `link`, `cfp`, `timezone`, `date`, `place`, `sub` attributes
+    + This [online web app](https://timezonefinder.michelfe.it/) makes it easy to find timezones – just click! 
+      (Based on [timezonefinder](https://github.com/jannikmi/timezonefinder) library)
+    + Alternatively, see available timezone strings [here](https://momentjs.com/timezone/).
+- Optionally add a `note`
+- Example:
+    ```yaml
+    - title: BestConf                          # Title of conference without year
+      year: 2022                               # Year
+      id: bestconf22                           # title as lower case + last two digits of year
+      full_name: Best Conference for Anything  # Full conference name (Optional)
+      link: link-to-website.com                # URL to conference
+      cfp_link: link-to-cfp.com                 # URL to call for proposals (Optional)
+      cfp: 'YYYY-MM-DD HH:mm:ss'               # Deadline for Call for Participation / Proposals
+      workshop_deadline: 'YYYY-MM-DD HH:mm:ss' # Workshop deadline if different from cfp (Optional)
+      tutorial_deadline: 'YYYY-MM-DD HH:mm:ss' # Tutorial deadline if different from cfp (Optional)
+      timezone: Asia/Seoul                     # Standard IANA Timezones (Omit for AoE)
+      place: Incheon, South Korea              # City, Country
+      date: September 18 - 22, 2022             # Nicely written dates of conference
+      start: YYYY-MM-DD                        # Start date of conference for calendar
+      end: YYYY-MM-DD                          # End date of conference for calendar
+      twitter: BestConfEver                    # Twitter handle of conference (Optional)
+      mastodon: https://mastodon.social/@bconf # Mastodon handle of conference (Optional)
+      sub: PY                                  # Type of conference (see or add _data/types.yml)
+      note: Important                          # In case there are extra notes about the conference (Optional)
+      location:                                # Geolocation for inclusion in map
+        latitude: 0.00
+        longitude: 0.00
+    ```
+- Send a pull request
+
+
+## Forks & other useful listings
+
+- [aideadlines][2] the original
+- [geodeadlin.es][3] by @LukasMosser
+- [neuro-deadlines][4] by @tbryn
+- [ai-challenge-deadlines][5] by @dieg0as
+- [CV-oriented ai-deadlines (with an emphasis on medical images)][8] by @duducheng
+- [es-deadlines (Embedded Systems, Computer Architecture, and Cyber-physical Systems)][9] by @AlexVonB and @k0nze
+- [2019-2020 International Conferences in AI, CV, DM, NLP and Robotics][10] by @JackieTseng
+- [ccf-deadlines][11] by @ccfddl
+- [netdeadlines.com][12] by @albertgranalcoz
+- [ad-deadlines.com][13] by @daniel-bogdoll
+
+## License
+
+This project is licensed under [MIT][1].
+
+It uses:
+
+- [IcoMoon Icons](https://icomoon.io/#icons-icomoon): [GPL](http://www.gnu.org/licenses/gpl.html) / [CC BY4.0](http://creativecommons.org/licenses/by/4.0/)
+
+[1]: https://abhshkdz.mit-license.org/
+[2]: http://aideadlin.es/
+[3]: https://github.com/LukasMosser/geo-deadlines
+[4]: https://github.com/tbryn/neuro-deadlines
+[5]: https://github.com/dieg0as/ai-challenge-deadlines
+[6]: http://www.conferenceranks.com/#
+[8]: https://m3dv.github.io/ai-deadlines/
+[9]: https://ekut-es.github.io/es-deadlines/
+[10]: https://jackietseng.github.io/conference_call_for_paper/conferences.html
+[11]: https://ccfddl.github.io/
+[12]: https://netdeadlines.com/
+[13]: https://ad-deadlines.com/
---
+++ utils/process.py
@@ -0,0 +1,220 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# Sort and Clean conference data.
+# It writes to `sorted_data.yml` and `cleaned_data.yml`, copy those to the conference.yml after screening.
+
+import datetime
+import pdb
+import sys
+import time
+import urllib
+from builtins import input
+from pathlib import Path
+from shutil import copyfile
+
+import pytz
+import requests
+import yaml
+from tqdm import tqdm
+
+try:
+    # for python newer than 2.7
+    from collections import OrderedDict
+except ImportError:
+    # use backport from pypi
+    from ordereddict import OrderedDict
+
+try:
+    from yaml import CDumper as Dumper
+    from yaml import CLoader as Loader
+except ImportError:
+    from yaml import Loader, Dumper
+
+from yaml.representer import SafeRepresenter
+
+_mapping_tag = yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG
+
+
+def dict_representer(dumper, data):
+    return dumper.represent_dict(data.iteritems())
+
+
+def dict_constructor(loader, node):
+    return OrderedDict(loader.construct_pairs(node))
+
+
+Dumper.add_representer(OrderedDict, dict_representer)
+Loader.add_constructor(_mapping_tag, dict_constructor)
+
+Dumper.add_representer(str, SafeRepresenter.represent_str)
+
+
+def ordered_dump(data, stream=None, Dumper=yaml.Dumper, **kwds):
+    class OrderedDumper(Dumper):
+        pass
+
+    def _dict_representer(dumper, data):
+        return dumper.represent_mapping(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG, data.items())
+
+    OrderedDumper.add_representer(OrderedDict, _dict_representer)
+    return yaml.dump(data, stream, OrderedDumper, **kwds)
+
+
+dateformat = "%Y-%m-%d %H:%M:%S"
+tba_words = ["tba", "tbd"]
+
+# Helper function for yes no questions
+def query_yes_no(question, default="no"):
+    """Ask a yes/no question via input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+
+
+def sort_by_cfp(data):
+    return pytz.utc.normalize(
+        datetime.datetime.strptime(data["cfp"], dateformat).replace(
+            tzinfo=pytz.timezone(
+                data.get("timezone", "AoE")
+                .replace("AoE", "Etc/GMT+12")
+                .replace("UTC+", "Etc/GMT-")
+                .replace("UTC-", "Etc/GMT+")
+            )
+        )
+    ).strftime(dateformat)
+
+
+def sort_by_date_passed(data):
+    right_now = datetime.datetime.utcnow().replace(microsecond=0).strftime(dateformat)
+    return sort_by_cfp(data) < right_now
+
+
+def split_data(data):
+    conf, tba, expired = [], [], []
+    for q in tqdm(data):
+        if q.get("end", datetime.datetime.utcnow().replace(microsecond=0).date()) < datetime.datetime.utcnow().replace(
+            microsecond=0
+        ).date() - datetime.timedelta(days=37):
+            expired.append(q)
+            continue
+
+        try:
+            if q["cfp"].lower() in tba_words:
+                tba.append(q)
+            else:
+                conf.append(q)
+        except:
+            print(data["cfp"].lower(), tba_words)
+    return conf, tba, expired
+
+
+def pretty_print(header, conf, tba=None, expired=None):
+    print(header)
+    for data in [conf, tba, expired]:
+        if data is not None:
+            for q in data:
+                print(q["cfp"], " - ", q["title"])
+            print("\n")
+
+
+def add_latlon(data):
+    for i, q in tqdm(enumerate(data.copy()), total = len(data)):
+        if ("location" in q) or ("place" not in q) and ("online" in q["place"].lower()):
+            continue
+        else:
+            url = "https://nominatim.openstreetmap.org/search/" + urllib.parse.quote(q["place"]) + "?format=json"
+            response = requests.get(url).json()
+            if response:
+                data[i]["location"] = {}
+                data[i]["location"]["latitude"] = float(response[0]["lat"])
+                data[i]["location"]["longitude"] = float(response[0]["lon"])
+                time.sleep(2)
+    return data
+
+
+# Sort:
+def sort_data(base=""):
+    url = Path(base, "_data", "conferences.yml")
+    out_url = Path(base, "utils", "sorted_conferences.yml")
+    archive = Path(base, "_data", "archive.yml")
+    out_archive = Path(base, "utils", "sorted_archive.yml")
+
+    with open(url, "r") as stream:
+        try:
+            data = yaml.load(stream, Loader=Loader)
+            
+            print("Initial Sorting:")
+            for q in data:
+                print(q["cfp"], " - ", q["title"])
+            print("\n\n")
+
+            # Geocode Data
+            print("Adding Lat Lon from Place")
+            data = add_latlon(data)
+
+            # Split data by cfp
+            print("Splitting conferences, tba, and archive")
+            conf, tba, expired = split_data(data)
+
+            # just sort:
+            conf.sort(key=sort_by_cfp, reverse=True)
+            pretty_print("Date Sorting:", conf, tba, expired)
+            conf.sort(key=sort_by_date_passed)
+            pretty_print("Date and Passed Deadline Sorting with tba:", conf, tba, expired)
+
+            with open(out_url, "w") as outfile:
+                for line in ordered_dump(
+                    conf + tba, Dumper=yaml.SafeDumper, default_flow_style=False, explicit_start=True
+                ).splitlines():
+                    outfile.write(line.replace("- title:", "\n- title:"))
+                    outfile.write("\n")
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    with open(archive, "r") as stream:
+        try:
+            data = yaml.load(stream, Loader=Loader)
+
+            data += expired
+
+            data.sort(key=sort_by_cfp, reverse=True)
+            data = add_latlon(data)
+
+            pretty_print("New archive:", data)
+            with open(out_archive, "w") as outfile:
+                for line in ordered_dump(
+                    data, Dumper=yaml.SafeDumper, default_flow_style=False, explicit_start=True
+                ).splitlines():
+                    outfile.write(line.replace("- title:", "\n- title:"))
+                    outfile.write("\n")
+        except yaml.YAMLError as exc:
+            print(exc)
+
+
+if __name__ == "__main__":
+    sort_data()
